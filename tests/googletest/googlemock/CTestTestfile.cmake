a20d50dfb32a810ec354425088d83f35e9e6b4bc
dllim <44658038+dllim@users.noreply.github.com>
2019-02-04 19:51:24 +0800

    Add files via upload

---
+++ tests/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/daryl/source/repos/ModularMadness/ModularMadness/Modular Madness/tests/googletest/googlemock
+# Build directory: C:/Users/daryl/source/repos/ModularMadness/ModularMadness/Modular Madness/tests/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ tests/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/daryl/source/repos/ModularMadness/ModularMadness/Modular Madness/tests/googletest/googletest
+# Build directory: C:/Users/daryl/source/repos/ModularMadness/ModularMadness/Modular Madness/tests/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
